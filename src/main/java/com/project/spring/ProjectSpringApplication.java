06c2fa86582c9e0ecab5526cae0a2306f7f39d82
Diegoabt18 <diegoalbertbt@gmail.com>
2022-06-30 21:31:18 -0500

    Inicio Estructura del Proyecto

---
+++ src/main/java/com/project/spring/ProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringApplication.class, args);
+	}
+
+}
